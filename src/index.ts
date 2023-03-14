03e67fa2625a457041ea1e0779664f35e76bf297
Mariko Ohtsuka <mariko.ohtsuka@nnh.go.jp>
2023-03-14 14:06:32 +0900

    delete files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app: express.Express = express();
-const port: number = 3000;
-app.get('/', (req, res) => {
-    res.send('hello~~~')
-});
-app.listen(port, () => {
-    console.log(`listening at http://localhost:${port}`);
-});
